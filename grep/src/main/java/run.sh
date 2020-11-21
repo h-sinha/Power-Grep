6bf19dc53df10cacd805efc3e86fb9404f9a0186
Harsh <harsh.26020@gmail.com>
2020-11-22 01:04:15 +0530

    Added mapper

--- grep/src/main/java/run.sh
+++ grep/src/main/java/run.sh
@@ -2,9 +2,9 @@ rm -rf out1
 rm com/ds/*.class
 /usr/local/hadoop/bin/hadoop com.sun.tools.javac.Main com/ds/Distributed.java
 jar cf run.jar com/ds/*.class
-time /usr/local/hadoop/bin/hadoop jar run.jar com.ds.Distributed input out1 india
+time /usr/local/hadoop/bin/hadoop jar run.jar com.ds.Distributed enwiki-latest-pages-articles26.xml-p42567204p42663461 out1 india
 cat out1/part*
 rm com/ds/*.class
 /usr/local/hadoop/bin/hadoop com.sun.tools.javac.Main com/ds/Sequential.java
 jar cf run.jar com/ds/*.class
-time /usr/local/hadoop/bin/hadoop jar run.jar com.ds.Sequential input out1 india
+time /usr/local/hadoop/bin/hadoop jar run.jar com.ds.Sequential enwiki-latest-pages-articles26.xml-p42567204p42663461 out1 india
